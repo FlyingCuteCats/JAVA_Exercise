794db94a5fd55cd9c50f2526218829b08c5553b2
leglath <eliotcore@gmail.com>
2023-09-07 16:32:34 +0100

    Add files via upload

---
+++ PrimePalindrome.java
@@ -0,0 +1,40 @@
+// Write a java program that takes in a number N and finds the smallest palindrome prime greater than or equal to N.
+
+import java.util.Scanner;
+public class PrimePalindrome {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int num = sc.nextInt();
+		System.out.println("The next prime palindrome is " + primePalin(num));
+		sc.close();
+	}
+
+	public static int primePalin(int para) {
+		while (isPrime(para) == false | isPalindrome(para) == false) {
+			para++;
+		}
+		return para;
+	}
+
+	public static boolean isPrime(int para) {
+		int ind = 2;
+		while (ind * ind < para) {
+			ind++;
+			if (para % ind == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isPalindrome(int para) {
+		String str = Integer.toString(para);
+		int length = str.length();
+		for (int i = 0; i < length / 2; i++) {
+			if (str.charAt(i) != str.chatAt(length - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
